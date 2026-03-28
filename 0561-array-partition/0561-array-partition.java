f807559240ca37a813d7c5f349aa48aa7e01a673
prathmesh kokare <prathmeshkokare009@gmail.com>
2026-03-28 19:03:44 +0530

    Time: 18 ms (32.16%), Space: 49.6 MB (66.74%) - LeetHub

---
+++ 0561-array-partition/0561-array-partition.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int arrayPairSum(int[] nums) {
+        Arrays.sort(nums);
+        int n= nums.length;
+        
+        int sum=0;
+        for(int i =0;i<nums.length;i+=2){
+         sum=sum+Math.min(nums[i],nums[i+1]);
+        }
+        return sum;
+
+        
+    }
+}
