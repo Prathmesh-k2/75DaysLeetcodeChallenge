80bb21caa20f9952305965a7331d1bb2c9c8f5bb
prathmesh kokare <prathmeshkokare009@gmail.com>
2026-06-01 18:25:19 +0530

    Time: 1589 ms (11.23%), Space: 0B (100%) - LeetHub

---
+++ 1341-movie-rating/1341-movie-rating.sql
@@ -0,0 +1,15 @@
+# Write your MySQL query statement below
+(select name as results
+from users
+inner join movieRating using(user_id)
+group by user_id
+order by count(rating) desc,name
+limit 1)
+union all
+(select title as results
+from movies
+inner join movieRating using (movie_id)
+WhERE MONTH(created_at)='02' and YEAR (created_at)='2020'
+group by title
+order by avg(rating) desc ,title
+limit 1)
